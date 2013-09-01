219da3af856d2f27c91bc801681e4e3bed204608
Vyacheslav <sadnerp@gmail.com>
2013-09-02 00:07:11 +0400

    add dynamic title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# returns title for page
+	def title(page_title)
+		base_title = "Education"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
